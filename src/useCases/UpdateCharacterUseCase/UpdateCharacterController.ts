8713b1c67a5ee6891e309b354160502d94a7e337
iago de sousa <iago.sousa@leanonsystems.com>
2021-12-20 14:08:35 -0300

    changed update id to query

--- src/useCases/UpdateCharacterUseCase/UpdateCharacterController.ts
+++ src/useCases/UpdateCharacterUseCase/UpdateCharacterController.ts
@@ -5,8 +5,9 @@ class UpdateCharacterController {
   constructor(private updateCharacterService: UpdateCharacterService) {}
 
   handle(req: Request, res: Response): Response {
-    const {name, id} = req.body
-    const character = this.updateCharacterService.execute({id, name});
+    const {name} = req.body
+    const {id} = req.query
+    const character = this.updateCharacterService.execute({id:id as string, name});
     return res.status(200).json(character).send();
   }
 }
